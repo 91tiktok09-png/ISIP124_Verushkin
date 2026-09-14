6ec3e064de5c3889a63891a5f89d6e95304275bc
91tiktok09-png <91tiktok09@gmail.com>
2026-09-14 14:24:52 +0300

    Меню

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp6
+namespace ExpenseTracker
 {
-    internal class Program
+    class Expense
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public double Amount { get; set; }
+
+        public Expense(string name, double amount)
         {
+            Name = name;
+            Amount = amount;
         }
     }
-}
+
+
+
+    ///
+    class Program
+    {
+        static List<Expense> expenses = new List<Expense>();
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Учет потраченных за день средств");
+            Console.WriteLine("--------------------------------");
+
+            int count = ReadOperationsCount();
+            ReadExpenses(count);
+
+            bool running = true;
+            while (running)
+            {
+                ShowMenu();
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        PrintData();
+                        break;
+                    case "2":
+                        ShowStatistics();
+                        break;
+                    case "3":
+                        BubbleSortByPrice();
+                        break;
+                    case "4":
+                        ConvertCurrency();
+                        break;
+                    case "5":
+                        SearchByName();
+                        break;
+                    case "0":
+                        running = false;
+                        Console.WriteLine("Выход из программы.");
+                        break;
+                    default:
+                        Console.WriteLine("Неверный пункт меню. Попробуйте снова.");
+                        break;
+                }
+            }
+        }
